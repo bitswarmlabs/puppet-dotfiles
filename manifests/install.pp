b0d553250c33ad3921049d613b59f93a2d795b77
Reuben Avery <ravery@bitswarm.io>
2016-07-06 12:11:16 -0400

    debugging

--- manifests/install.pp
+++ manifests/install.pp
@@ -32,6 +32,13 @@ define dotfiles::install(
     logoutput => true,
   }
   ~>
+  exec { "backup ${name} zshrc":
+    command => "cp -f ${home}/.zshrc ${home}/.zshrc.orig",
+    path    => ['/bin', '/usr/bin'],
+    user    => $name,
+    onlyif  => "test ! -d ${home}/.yadr && test -e ${home}/.zshrc"
+  }
+  ~>
   ruby::rake { "dotfiles:yadr:${name} rake install":
     cwd         => "${home}/.yadr",
     task        => 'install',
@@ -40,23 +47,24 @@ define dotfiles::install(
     refreshonly => true,
   }
   ->
-  anchor { "dotfiles:install:${name}:end": }
+  Anchor["dotfiles:install:${name}:end"]
 
-  exec { "backup ${name} zshrc":
-    command => "cp -f ${home}/.zshrc ${home}/.zshrc.orig",
-    path    => ['/bin', '/usr/bin'],
-    user    => $name,
-    onlyif  => "test ! -d ${home}/.yadr && test -e ${home}/.zshrc"
+  if $_set_sh {
+    if str2bool($ohmyzsh::config::manage_user) and ! defined(User[$name]) {
+      user { "ohmyzsh::user ${name}":
+        ensure     => present,
+        name       => $name,
+        managehome => true,
+        shell      => $ohmyzsh::config::zsh,
+        require    => Package[$ohmyzsh::config::zsh_package_name],
+      }
+      ~>Anchor["dotfiles:install:${name}:end"]
+    } else {
+      User <| title == $name |> {
+        shell => $ohmyzsh::config::zsh
+      }
+    }
   }
-  # ~>
-  # exec { "dotfiles::cp .zshrc ${name}":
-  #   creates => "${home}/.zshrc",
-  #   command => "cp ${home}/.yadr/templates/zshrc.zsh-template ${home}/.zshrc",
-  #   path    => ['/bin', '/usr/bin'],
-  #   onlyif  => "getent passwd ${name} | cut -d : -f 6 | xargs test -e",
-  #   user    => $name,
-  #   require => Exec["dotfiles::git clone ${name}"],
-  #   before  => File_line["dotfiles::disable_auto_update ${name}"],
-  # }
 
+  anchor { "dotfiles:install:${name}:end": }
 }
